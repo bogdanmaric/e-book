a614c41fe59006310bbc74e951808876694220fb
SkyLightCode <67342565+SkyLightCode@users.noreply.github.com>
2022-11-14 18:19:47 +0100

    Creating navigation.blade.php

---
+++ resources/views/pages/layouts/navigation.blade.php
@@ -0,0 +1,12 @@
+<nav class="navbar navbar-dark justify-content-between">
+    <a class="navbar-brand" href="#">
+        <img src="{{asset("assets/images/ebook.png")}}" width="30" height="30" class="d-inline-block align-top" alt="">
+        E-Book Prodavnica
+    </a>
+
+    <div class="right-menu">
+        <a href="#">Registruj se</a>
+        <a href="#">Prijavi se</a>
+        <p class="mb-0" id="user-name">Posetilac</p>
+    </div>
+</nav>
