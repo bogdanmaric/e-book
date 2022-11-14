b071dad25c5ea877627a4c7a92a67aaecde547e7
SkyLightCode <67342565+SkyLightCode@users.noreply.github.com>
2022-11-14 18:25:59 +0100

    Changes in web.php: adding Route::resource for BookControllerR(index) and redirecting / to /ebook

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect("/ebook");
 });
+
+Route::resource("/ebook", App\Http\Controllers\BookControllerR::class);
