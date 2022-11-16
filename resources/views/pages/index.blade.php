f83b688788a6adb483755a84aad497a4d2457dcf
SkyLightCode <67342565+SkyLightCode@users.noreply.github.com>
2022-11-16 16:42:22 +0100

    Changes in index.blade.php: correcting little mistakes

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -8,9 +8,10 @@
 
 @section("main")
     <div class="container">
-        <div class="row books jus text-center justify-content-center">
+        <div class="row books text-center justify-content-center">
         @foreach ($books as $book)
                 <div class="book card col-12 col-md-4 p-3 m-5">
+                    <a style="text-decoration: none; color: black;" href="{{route("ebook.show", $book->id)}}">
                     <img class="card-img-top book-image" src="{{asset("assets/images/cover.jpg")}}" alt="book-image">
                     <div class="card-body book-body">
                         <h5 class="card-title book-title">{{$book->title}}</h5>
@@ -26,6 +27,7 @@
                             </button>
                         </form>
                     </div>
+                    </a>
                 </div>
 
 {{--            <div class="col-12 col-md-4">
