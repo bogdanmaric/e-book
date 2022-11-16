1d04216788668f386bf42d8bd2766e12ff6e277f
SkyLightCode <67342565+SkyLightCode@users.noreply.github.com>
2022-11-16 16:41:24 +0100

    Changes in navigation.blade.php: setting link on logo for redirecting to home page

--- resources/views/pages/layouts/navigation.blade.php
+++ resources/views/pages/layouts/navigation.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-dark justify-content-between">
-    <a class="navbar-brand" href="#">
+    <a class="navbar-brand" href="{{url("/")}}">
         <img src="{{asset("assets/images/ebook.png")}}" width="30" height="30" class="d-inline-block align-top" alt="">
         E-Book Prodavnica
     </a>
