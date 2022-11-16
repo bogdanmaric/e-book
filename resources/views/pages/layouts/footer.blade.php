772bc43f816b1e6923f2afed4c9224175e361d98
SkyLightCode <67342565+SkyLightCode@users.noreply.github.com>
2022-11-16 12:45:49 +0100

    Changes in footer.blade.php and footer.css: changing tag <p> to <small> and setting id, also changing tag selection to id name "copyright"

--- resources/views/pages/layouts/footer.blade.php
+++ resources/views/pages/layouts/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="navbar fixed-bottom">
-    <p class="p-0 m-0">Copyright &copy; Bogdan Marić 2022</p>
+    <small class="p-0 m-0" id="copyright">Copyright &copy; Bogdan Marić 2022</small>
 </footer>
