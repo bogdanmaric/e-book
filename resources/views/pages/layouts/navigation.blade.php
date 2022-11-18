1ecfc91c91613d10a7c34ae8688a7584a21206d3
SkyLightCode <67342565+SkyLightCode@users.noreply.github.com>
2022-11-18 19:58:00 +0100

    Changes in navigation.blade.php: adding navigation links for "adding book" and "logout" that appears when admin is logged in

--- resources/views/pages/layouts/navigation.blade.php
+++ resources/views/pages/layouts/navigation.blade.php
@@ -5,6 +5,19 @@
     </a>
 
     <div class="right-menu">
-        <a href="#"><span><img id="cart-image" src="{{asset("assets/images/cart-64.png")}}"></span></a>
+        <a href="#" id="cart"><span><img id="cart-image" src="{{asset("assets/images/cart-64.png")}}"></span></a>
+        @if (Auth::check())
+            <a class="nav-link" id="add-book" href="#">Dodaj Knjigu</a>
+
+            <!-- Authentication -->
+            <form id="logout" method="POST" action="{{ route('logout') }}">
+                @csrf
+                <a href="route('logout')"
+                onclick="event.preventDefault();
+                this.closest('form').submit();">Odjava</a>
+            </form>
+        @endif
+
+
     </div>
 </nav>
