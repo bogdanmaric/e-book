e9226a4d00d1e43422b94b6554aa5ec886b25637
SkyLightCode <67342565+SkyLightCode@users.noreply.github.com>
2022-11-19 14:44:54 +0100

    Changes in navigation.blade.php and navigation.css: adding icons for "logout" and "add book", and styling in navigation.css

--- resources/views/pages/layouts/navigation.blade.php
+++ resources/views/pages/layouts/navigation.blade.php
@@ -7,15 +7,20 @@
     <div class="right-menu">
         <a href="#" id="cart"><span><img id="cart-image" src="{{asset("assets/images/cart-64.png")}}"></span></a>
         @if (Auth::check())
-            <a class="nav-link" id="add-book" href="#">Dodaj Knjigu</a>
+            <div id="add-book">
+                <a href="#">Dodaj Knjigu</a>
+                <img src="{{asset("assets/images/plus-sign.png")}}" width="30" height="30" class="d-inline-block align-top" alt="">
+            </div>
 
-            <!-- Authentication -->
-            <form id="logout" method="POST" action="{{ route('logout') }}">
-                @csrf
-                <a href="route('logout')"
-                onclick="event.preventDefault();
-                this.closest('form').submit();">Odjava</a>
-            </form>
+            <div id="logout">
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <a href="route('logout')"
+                    onclick="event.preventDefault();
+                    this.closest('form').submit();">Odjava</a>
+                </form>
+                <img src="{{asset("assets/images/power.png")}}" width="30" height="30" class="d-inline-block align-top" alt="">
+            </div>
         @endif
 
 
