cb39673ccb33c217f4a1f437ab5fdf2a19b07a1f
SkyLightCode <67342565+SkyLightCode@users.noreply.github.com>
2022-11-21 23:34:10 +0100

    Changes category-management.blade.php: remaking table structure and style

--- resources/views/pages/category-management.blade.php
+++ resources/views/pages/category-management.blade.php
@@ -3,59 +3,54 @@
 @section("title", "E-book")
 
 @section("style")
-    <link rel="stylesheet" href="{{asset("assets/css/book.css")}}">
+    <link rel="stylesheet" href="{{asset("assets/css/category-management.css")}}">
 @endsection
 
 @section("main")
-    <h1 style="display: flex;justify-content: center; color: #024499;">Lista Kategorija</h1>
-    <div class="ui placeholder segment inline-block">
-        <div class="column">
-            <!-- Tabela korisnici -->
-            <div style="display: flex; justify-content: center; margin-bottom: 3px;">
-                <form class="kreiranje-kategorije"
-                      action="{{ action("\App\Http\Controllers\CategoryControllerR@store") }}"
+    <table class="table mt-5">
+        <thead class="thead-dark">
+        <tr>
+            <th scope="row" colspan="3">
+                    <form class="kreiranje-kategorije"
+                          action="{{ action("\App\Http\Controllers\CategoryControllerR@store") }}"
+                          method="post">
+                        @csrf
+                        <input type="text" name="category_name" placeholder="Naziv kategorije" required>
+                        <input class="btn btn-primary" type="submit" value="Kreiraj novu kategoriju">
+                    </form>
+            </th>
+        </tr>
+        <tr>
+            <th scope="col">Naziv kategorije</th>
+            <th scope="col">Promena naziva kategorije</th>
+            <th scope="col">Izbriši kategoriju</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($categories as $category)
+        <tr>
+            <td>{{$category->name}}</td>
+            <td>
+                <form class="change-category"
+                       action="{{ action("\App\Http\Controllers\CategoryControllerR@update", [$category->id]) }}"
+                       method="post">
+                    <input type="text" name = "category_name" placeholder="Novi naziv kategorije" required>
+                    @csrf
+                    @method("PUT")
+                    <input class="btn btn-warning" type="submit" value="Izmeni">
+                </form>
+            </td>
+            <td>
+                <form class="delete-category"
+                      action="{{ action("\App\Http\Controllers\CategoryControllerR@destroy", [$category->id]) }}"
                       method="post">
                     @csrf
-                    <input type="text" name="category_name" placeholder="Kreiraj novu kategorija" required>
-                    <input type="submit" value="Kreiraj kategoriju">
+                    @method("DELETE")
+                    <input class="btn btn-danger" type="submit" value="Izbriši">
                 </form>
-            </div>
-            <table>
-                <tr>
-                    <th>Naziv kategorije</th>
-                    <th>Promena naziv kategorije</th>
-                    <th>Izbriši kategoriju</th>
-                </tr>
-
-                @foreach($categories as $category)
-                    <tr>
-                        <td id="naziv_kategorije">{{ $category->name }}</td>
-
-                        <td>
-                            <form class="promena-kategorije"
-                                  action="{{ action("\App\Http\Controllers\CategoryControllerR@update", [$category->id]) }}"
-                                  method="post">
-                                <input type="text" name = "category_name" placeholder="Naziv" required>
-                                @csrf
-                                @method("PUT")
-                                <input type="submit" value="Promeni">
-                            </form>
-                        </td>
-
-
-                        <td>
-                            <form class="brisanje-kategorije"
-                                  action="{{ action("\App\Http\Controllers\CategoryControllerR@destroy", [$category->id]) }}"
-                                  method="post">
-                                @csrf
-                                @method("DELETE")
-                                <input type="submit" value="Izbriši">
-                            </form>
-                        </td>
-                    </tr>
-                @endforeach
-
-            </table>
-        </div>
-    </div>
+            </td>
+        </tr>
+        @endforeach
+        </tbody>
+    </table>
 @endsection
