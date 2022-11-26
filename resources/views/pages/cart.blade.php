13e9aee2b5c846bb2ff36f1b058dbbe95445de2f
SkyLightCode <67342565+SkyLightCode@users.noreply.github.com>
2022-11-26 15:15:51 +0100

    Changes in cart.blade.php: checking if cart is empty, looping through cart and displaying books, styling with bootstrap

--- resources/views/pages/cart.blade.php
+++ resources/views/pages/cart.blade.php
@@ -7,31 +7,44 @@
 @endsection
 
 @section("main")
-    @if($cart->isEmpty())
-        <div class="col">
-            <p id="text-prazna-korpa" class="text-center mt-5">Vaša korpa je prazna</p>
-        </div>
-        <div class="col text-center mt-5">
-            <img id="slika-prazna-korpa" class="book-cart mt-5" src="{{asset("assets/images/cart-empty.png")}}">
-        </div>
-    @else
+{{--    @if($cart->isEmpty())--}}
+{{--        <div class="col">--}}
+{{--            <p id="text-prazna-korpa" class="text-center mt-5">Vaša korpa je prazna</p>--}}
+{{--        </div>--}}
+{{--        <div class="col text-center mt-5">--}}
+{{--            <img id="slika-prazna-korpa" class="book-cart mt-5" src="{{asset("assets/images/cart-empty.png")}}">--}}
+{{--        </div>--}}
+{{--    @else--}}
     <table class="table table-bordered mt-5">
         <thead>
         <tr>
-            <th scope="col" colspan="3">Knjiga</th>
-            <th scope="col">Cena</th>
-            <th scope="col">Izbriši</th>
+            <th scope="col" class="text-center">Knjiga</th>
+            <th scope="col" class="text-center">Cena</th>
+            <th scope="col" class="text-center">Izbriši</th>
         </tr>
         </thead>
         <tbody>
-        @foreach($cart as $book)
+        @foreach($books as $book)
             <tr>
-                <td colspan="3">Larry the Bird</td>
-                <th>3</th>
-                <td>@twitter</td>
+                <th scope="row" class="d-flex justify-content-around">
+                    <div class="d-flex flex-column">
+                        <p>Naslov: {{$book->title}}</p>
+                        <p>Autor: {{$book->author}}</p>
+                        <p>Izdavač: {{$book->publisher}}</p>
+                    </div>
+                    <img style="border: solid;border-radius: 20px;" width="200" src="{{$book->image_link}}">
+                </th>
+                <td>{{$book->price}}</td>
+                <td>
+                    <form action="$" method="post">
+                        @csrf
+                        @method("DELETE")
+                        <button class="btn btn-danger">Ukloni iz korpe</button>
+                    </form>
+                </td>
             </tr>
         @endforeach
         </tbody>
     </table>
-    @endif
+{{--    @endif--}}
 @endsection
