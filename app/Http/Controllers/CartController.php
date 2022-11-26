4b502ca3c753b39e90ac2b637a5b1e2c83384995
SkyLightCode <67342565+SkyLightCode@users.noreply.github.com>
2022-11-26 15:13:34 +0100

    Changes in CartController.php: get books IDs from session, finding books by these IDs and passing theme to cart.blade.php

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
 use Illuminate\Http\Request;
 use MongoDB\Driver\Session;
 
 class CartController extends Controller
 {
     public function index() {
-        $cart = session()->get("cart");
-        return view("pages.cart", compact("cart"));
+        $booksInCartIdArray = session()->get("cart");
+        $books = Book::findMany($booksInCartIdArray);
+        return view("pages.cart", compact("books"));
     }
 }
